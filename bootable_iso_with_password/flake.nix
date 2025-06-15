2634d4aaf24bbc038d02abccc97b176cc845ca54
Marcel Novotny <marceln@xcroco.com>
2025-06-15 22:22:47 +0200

    flake empty

--- bootable_iso_with_password/flake.nix
+++ bootable_iso_with_password/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Minimal NixOS installation media";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
       exampleIso = nixpkgs.lib.nixosSystem {
