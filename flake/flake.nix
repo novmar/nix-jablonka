4f6046ce3cca7d85e801ebaa50f87cee6f6702c6
Marcel Novotny <marceln@xcroco.com>
2025-06-16 00:24:07 +0200

    Flake

---
+++ flake/flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Manage NixOS server remotely";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    colmena.url = "github:zhaofengli/colmena";
+    disko ={
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, disko, colmena, ... }:
+    {
+      nixosConfigurations.demo = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./configuration.nix
+        ];
+      };
+      colmenaHive = colmena.lib.makeHive {
+        meta = {
+          nixpkgs = nixpkgs.legacyPackages.x86_64-linux;
+        };
+
+        defaults = { pkgs, ... }: {
+          environment.systemPackages = [
+            pkgs.curl
+          ];
+        };
+        demo = { pkgs, ... }: {
+          deployment = {
+            targetHost = "demo";
+            targetPort = 22;
+            targetUser = "jablonka";
+            buildOnTarget = true;
+            tags = [ "homelab" ];
+          };
+          nixpkgs.system = "x86_64-linux";
+          imports = [
+            disko.nixosModules.disko
+            ./configuration.nix
+          ];
+          time.timeZone = "Europe/Prague";
+        };
+      };
+    };
+}
